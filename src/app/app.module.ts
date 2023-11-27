675fab379aa6a8151da2da1eaba6abfa3bc9e7d5
Sergio Morales <smoralesr@udistrital.edu.co>
2023-11-27 10:31:12 -0500

    Creacion componente reservas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerPromocionesComponent } from './banner-promociones/banner-promociones.component';
+import { ReservasComponent } from './reservas/reservas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerPromocionesComponent
+    BannerPromocionesComponent,
+    ReservasComponent
   ],
   imports: [
     BrowserModule,
